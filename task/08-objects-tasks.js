1274e7b4f8620f994e641a50c1bc763c25b6a344
Gautam Gupta <gautam_gupta@epam.com>
2023-03-12 14:31:44 +0530

    Coding problems

---
+++ task/08-objects-tasks.js
@@ -0,0 +1,79 @@
+'use strict';
+
+/**************************************************************************************************
+ *                                                                                                *
+ * Plese read the following tutorial before implementing tasks:                                   *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Object_initializer *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object        *
+ *                                                                                                *
+ **************************************************************************************************/
+
+
+/**
+ * Returns the rectagle object with width and height parameters and getArea() method
+ *
+ * @param {number} width
+ * @param {number} height
+ * @return {Object}
+ *
+ * @example
+ *    var r = new Rectangle(10,20);
+ *    console.log(r.width);       // => 10
+ *    console.log(r.height);      // => 20
+ *    console.log(r.getArea());   // => 200
+ */
+function Rectangle(width, height) {
+      
+    this.width = width;
+    this.height = height;
+    
+}
+  Rectangle.prototype.getArea = function(){
+    return this.width*this.height;
+}
+
+/**
+ * Returns the JSON representation of specified object
+ *
+ * @param {object} obj
+ * @return {string}
+ *
+ * @example
+ *    [1,2,3]   =>  '[1,2,3]'
+ *    { width: 10, height : 20 } => '{"height":10,"width":20}'
+ */
+function getJSON(obj) {
+  return JSON.stringify(obj);
+}
+
+
+/**
+ * Returns the object of specified type from JSON representation
+ *
+ * @param {Object} proto
+ * @param {string} json
+ * @return {object}
+ *
+ * @example
+ *    var r = fromJSON(Rectangle.prototype, '{"width":10, "height":20}');
+ *
+ */
+function fromJSON(proto, json) {
+    
+        const obj = JSON.parse(json);
+        const newObj = Object.create(proto);
+        Object.assign(newObj,obj)
+       
+        return newObj;
+        
+    }
+
+
+
+
+module.exports = {
+    Rectangle: Rectangle,
+    getJSON: getJSON,
+    fromJSON: fromJSON,
+  
+};
---
+++ test/08-objects-tests.js
@@ -0,0 +1,106 @@
+'use strict';
+
+var assert = require('assert');
+var tasks = require('../task/08-objects-tasks');
+it.optional = require('../extensions/it-optional');
+
+describe('08-objects-tasks', function() {
+
+    it.optional('Rectangle constructor should return the rectangle object', function () {
+        var rect = new tasks.Rectangle(10,20);
+
+        assert.equal(
+            typeof rect,
+            'object',
+            'Result of Rectangle constructor should be an object'
+        );
+        assert(
+            rect.hasOwnProperty('width'),
+            'Result of Rectangle constructor should be an object with "width" property'
+        );
+        assert.equal(
+            rect.width,
+            10,
+            'Result of new Rectangle(10,20) should be an object with "width" property equals to 10'
+        );
+        assert(
+            rect.hasOwnProperty('height'),
+            'Result of new Rectangle(10,20) should be an object with "height" property'
+        );
+        assert.equal(
+            rect.width,
+            10,
+            'Result of new Rectangle(10,20) should be an object with "height" property equals to 20'
+        );
+        assert.equal(
+            typeof rect.getArea,
+            'function',
+            'Result of Rectangle constructor should be an object with "getArea" method'
+        );
+        assert.equal(
+            rect.getArea(),
+            200,
+            'Result of (new Rectangle(10,20)).getArea() should return the correct area of specified rectangle'
+        );
+        assert.equal(
+            (new tasks.Rectangle(3,8)).getArea(),
+            24,
+            'Result of (new Rectangle(3,8)).getArea() should return the correct area of specified rectangle'
+        );
+    });
+
+
+    it.optional('getJSON should return the JSON representation of specified object', function () {
+        [
+            {
+                obj: [ 1, 2, 3],
+                expected: '[1,2,3]'
+            },{
+                obj: { height: 10, width: 20 },
+                expected: '{"height":10,"width":20}'
+            }
+        ].forEach(data => {
+            assert.equal(
+                tasks.getJSON(data.obj),
+                data.expected
+            );
+        });
+    });
+
+
+    it.optional('fromJSON should return the object of specified type from JSON representation', function () {
+        var MockType = function(a,b,c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        };
+
+        [
+            {
+                proto: tasks.Rectangle.prototype,
+                json: '{ "width":10, "height":20 }',
+                expected: new tasks.Rectangle(10, 20)
+            },{
+                proto: MockType.prototype,
+                json: '{ "a":10, "b":20, "c":30 }',
+                expected: new MockType(10,20,30)
+            }
+        ].forEach(data => {
+            var actual = tasks.fromJSON(data.proto, data.json);
+            assert.deepEqual(
+                actual,
+                data.expected,
+                'fromJson method shoud restore all properties from json'
+            );
+            assert.equal(
+                actual.__proto__,
+                data.expected.__proto__,
+                'fromJson method shoud restore type from prototype argument'
+            );
+        });
+    });
+
+
+    
+
+});
